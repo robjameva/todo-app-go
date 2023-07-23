1acf2afbd5527d181ee29d68a3627682532536b1
Robert Evanik <rob.evanik@gmail.com>
2023-07-23 08:16:33 -0400

    go & ts project setup

---
+++ server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Print("hello world")
+
+	app := fiber.New()
+
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	log.Fatal(app.Listen(":4000"))
+
+}
